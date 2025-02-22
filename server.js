dd106d82799f2481d3ba028dbdaa45f72fac4059
Mihir <you@example.com>
2025-02-22 05:56:38 -0800

    save

--- server.js
+++ server.js
@@ -36,14 +36,14 @@ app.get("/login", (req, res) => res.send("login is running ✅"));
 
 
 app.post("/register", async (req, res) => {
-  const { username, email, password } = req.body;
-  if (!username || !email || !password) return res.status(400).json({ error: "All fields are required" });
+  const { username, email, password , phone} = req.body;
+  if (!username || !email || !password || !phone) return res.status(400).json({ error: "All fields are required" });
 
   const existingUser = await User.findOne({ email });
   if (existingUser) return res.status(400).json({ error: "Email already in use" });
 
   const hashedPassword = await bcrypt.hash(password, 10);
-  await new User({ username, email, password: hashedPassword }).save();
+  await new User({ username, email, password: hashedPassword , address ,phone }).save();
   res.status(201).json({ message: "User registered successfully" });
 });
 
