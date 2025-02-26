915d751abf68cd8f1cff160717e4692214820392
Mihir <you@example.com>
2025-02-25 21:43:41 -0800

    save

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const cors = require("cors");
-const authRoutes = require("./routes/authRoutes"); // Import Routes
+const authRoutes = require("./Routes/authRoutes"); // Import Routes
 
 dotenv.config();
 const app = express();  
